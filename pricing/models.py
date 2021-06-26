fb0696d2a60dc3d0c31c5cde892caa1dff4d8f92
mnogoruk <lionless072@gmail.com>
2021-06-26 20:57:01 +0300

    route services models and serializers

--- pricing/models.py
+++ pricing/models.py
@@ -35,3 +35,21 @@ class ZoneRate(AbstractRate):
 
     def __str__(self):
         return f"{self.zone}: {self.range_from} - {self.range_to} ({self.price_per_unit})"
+
+class ServiceAdditional(AbstractCreate):
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    currency = models.CharField(max_length=12, default=Currency.EURO.value)
+    route = models.ForeignKey(HubRoute, on_delete=models.CASCADE, related_name='additional_services')
+    type = 'ADDITIONAL'
+
+    def __str__(self):
+        return f"{self.route}: {self.name}"
+
+class ServiceRanked(AbstractRate):
+    name = models.CharField(max_length=120)
+    route = models.ForeignKey(HubRoute, on_delete=models.CASCADE, related_name='ranked_services')
+    type = "RANKED"
+
+    def __str__(self):
+        return f"{self.route}: {self.name}"
--- pricing/serializers.py
+++ pricing/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from geo.models import Zone
-from pricing.models import RouteRate, ZoneRate
+from pricing.models import RouteRate, ZoneRate, ServiceAdditional, ServiceRanked
 
 
 class RouteRateSerializer(serializers.ModelSerializer):
@@ -16,3 +16,14 @@ class ZoneRateSerializer(serializers.ModelSerializer):
         model = ZoneRate
         exclude = ['zone', 'created_at']
 
+class ServiceAdditionalSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ServiceAdditional
+        exclude = ['route', 'created_at']
+
+class ServiceRankedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ServiceRanked
+        exclude = ['route', 'created_at']
