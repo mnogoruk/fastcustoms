0a0eb48c69bbebc855120e57e9e4a7122ef20ceb
mnogoruk <lionless072@gmail.com>
2021-07-08 22:20:33 +0300

    geo app moved to new schema

--- geo/filters.py
+++ geo/filters.py
@@ -8,12 +8,27 @@ class CityFilter(filters.BaseFilterBackend):
         state_name = params.get('state')
         country_name = params.get('country')
         zone_name = params.get('zone')
-        
+
         if state_name:
             queryset = queryset.filter(state__name=state_name)
         if country_name:
             queryset = queryset.filter(state__country__name=country_name)
         if zone_name:
-            queryset = queryset.filter(state__country__zone__name=zone_name)
+            queryset = queryset.filter(state__zone__name=zone_name)
+
+        return queryset.order_by('name')
+
+
+class StateFilter(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        params = request.query_params
+
+        country_name = params.get('country')
+        zone_name = params.get('zone')
+
+        if country_name:
+            queryset = queryset.filter(country__name=country_name)
+        if zone_name:
+            queryset = queryset.filter(zone__name=country_name)
 
         return queryset.order_by('name')
